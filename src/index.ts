5cd687f777cba98f0de434479f09471a1c3c9bab
ggang89 <ggang89@gmail.com>
2024-11-23 18:11:05 +0900

    함수 타입 지정

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 let todoItems;
 
-// api
-function fetchTodoItems() {
+// fetchTodoItems() 함수가 todos라는 배열이 담긴 변수를 변환하고 있기 때문에 함수의 반환타입은 todos 변수의 데이터 타입이어야 함
+function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -10,31 +10,33 @@ function fetchTodoItems() {
   return todos;
 }
 
-// crud methods
-function fetchTodos() {
+// fetchTodos는 fetchTodoItems()함수의 반환값을 변수에 담아서 반환하므로 동일한 반환 타입을 가진다.
+function fetchTodos(): { id: number; title: string; done: boolean }[] {
   const todos = fetchTodoItems();
   return todos;
 }
-
-function addTodo(todo) {
+//todo에는 현재 아무 값이나 올 수 있다.
+//함수의 반환값 없으면 void
+function addTodo(todo: any): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: any): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: any, todo: any): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
-// business logic
-function logFirstTodo() { //첫 번째 할 일 출력
+//첫 번째 할 일 출력
+function logFirstTodo(): any {
   return todoItems[0];
 }
 
-function showCompleted() { // 완료된 할일이 목록 표시
+// 완료된 할 일 목록 표시
+function showCompleted(): any {
   return todoItems.filter(item => item.done);
 }
 
