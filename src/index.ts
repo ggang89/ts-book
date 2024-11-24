72ca12886d44e26d7d6b7aebd19681462841d3b7
ggang89 <ggang89@gmail.com>
2024-11-24 16:53:54 +0900

    중복타입을 interface로 정의하고 교체

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-let todoItems: { id: number; title: string; done: boolean }[];
+interface ToDo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: ToDo[];
 
 // fetchTodoItems() 함수가 todos라는 배열이 담긴 변수를 변환하고 있기 때문에 함수의 반환타입은 todos 변수의 데이터 타입이어야 함
-function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
+function fetchTodoItems(): ToDo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,13 +17,13 @@ function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
 }
 
 // fetchTodos는 fetchTodoItems()함수의 반환값을 변수에 담아서 반환하므로 동일한 반환 타입을 가진다.
-function fetchTodos(): { id: number; title: string; done: boolean }[] {
+function fetchTodos(): ToDo[] {
   const todos = fetchTodoItems();
   return todos;
 }
 //todo에는 현재 아무 값이나 올 수 있다.
 //함수의 반환값 없으면 void
-function addTodo(todo: { id: number; title: string; done: boolean }): void {
+function addTodo(todo: ToDo): void {
   todoItems.push(todo);
 }
 
@@ -25,27 +31,26 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(
-  index: number,
-  todo: { id: number; title: string; done: boolean }
-): void {
+function completeTodo(index: number, todo: ToDo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 //첫 번째 할 일 출력
-function logFirstTodo(): { id: number; title: string; done: boolean } {
+function logFirstTodo(): ToDo {
   return todoItems[0];
 }
 
 // 완료된 할 일 목록 표시
-function showCompleted(): { id: number; title: string; done: boolean }[] {
+function showCompleted(): ToDo[] {
   return todoItems.filter(item => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  addTodo({ id: 4, title: '타입 정의', done: false });
+  addTodo({ id: 5, title: '복습', done: false });
 }
 
 // NOTE: 유틸 함수
